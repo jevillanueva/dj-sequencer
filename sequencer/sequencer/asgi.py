15e0ab477f9384cf831793ac442ee1dd26df9e65
Jonathan Villanueva <me@jevillanueva.dev>
2024-06-11 22:20:03 -0400

    refactor: :fire: Reorder folders and files
    Rename folder root and files

--- sequencer/sequencer/asgi.py
+++ sequencer/sequencer/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for documentSequencer project.
+ASGI config for sequencer project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'documentSequencer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sequencer.settings')
 
 application = get_asgi_application()
